ae04e05d97b641d10d5c51c86b6b0e7007d8f67a
Prateek Sharma <pratzz.sharma@gmail.com>
2022-02-13 03:34:19 +0530

    Create Constants.kt

---
+++ app/src/main/java/com/lifesum/common/Constants.kt
@@ -0,0 +1,16 @@
+package com.lifesum.common
+
+
+object Constants {
+
+    const val ZERO_DOUBLE = 0.0
+    const val ZERO = 0
+    const val EMPTY_STRING = ""
+}
+object URLConstants{
+    const val BASE_URL = "https://api.lifesum.com/v2/foodipedia/"
+    const val FOOD_URL = "codetest"
+    const val QUERY_FOOD_ID = "foodid"
+    const val AUTHORIZATION = "Authorization"
+    const val AUTH_TOKEN = "23863708:465c0554fd00da006338c72e282e939fe6576a25fd00c776c0fbe898c47c9876"
+}
