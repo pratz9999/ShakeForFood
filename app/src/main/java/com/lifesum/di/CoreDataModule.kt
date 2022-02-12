2c89f1554532a372354feabc96bf76994e8d50ea
Prateek Sharma <pratzz.sharma@gmail.com>
2022-02-13 03:34:04 +0530

    Create CoreDataModule.kt

---
+++ app/src/main/java/com/lifesum/di/CoreDataModule.kt
@@ -0,0 +1,44 @@
+package com.lifesum.di
+
+
+import com.lifesum.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.*
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+
+/**
+ * Dagger module to provide core data functionality.
+ */
+
+@InstallIn(SingletonComponent::class)
+@Module
+class CoreDataModule {
+
+    @ServerApi
+    @Provides
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(interceptor)
+            .build()
+
+    @Provides
+    fun provideLoggingInterceptor() =
+        HttpLoggingInterceptor().apply { level = if (BuildConfig.DEBUG) BODY else NONE }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides
+    @Singleton
+    fun provideMoshiConverterFactory(moshi: Moshi): MoshiConverterFactory =
+        MoshiConverterFactory.create(moshi).asLenient()
+}
